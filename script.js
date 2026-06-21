38fe09936cdd1ad7402e2fefab9d19ba1c812b5e
Aaraghya <aaraghya.bhatnagar@gmail.com>
2026-06-22 03:44:44 +0530

    animation

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const logo = document.getElementById("dvd-logo");
+//curr
+let x = 100;
+let y = 100;
+// peed
+let speedX = 2;
+let speedY = 2;
 
+logo.style.position = "absolute";
+
+function animate() {
+
+    x += speedX;
+    y += speedY;
+
+    logo.style.left = x + "px";
+    logo.style.top = y + "px";
+
+    requestAnimationFrame(animate);
+}
+
+animate();
